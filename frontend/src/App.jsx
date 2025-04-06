45e3ca6cad16b5da524bf64e85c06ce1872b1612
Ashank007 <guptaashank38@gmail.com>
2025-04-06 10:50:37 +0530

    Added Frontend Dashboard and More

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./login";
 import Register from "./register";
 import "./App.css";
-
+import Dashboard from "./Dashboard";
+import EditProfile from "./EditProfile";
+import AddInterest from "./AddInterest";
+import ChooseCourses from "./ChooseCourses";
+import AdminLogin from "./AdminLogin";
+import AdminDashboard from "./AdminDashboard";
+import ViewUsers from "./ViewUser";
 export default function App() {
   return (
     <Router>
@@ -11,6 +17,13 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
+	  <Route path="/dashboard" element={<Dashboard />} />"
+	  <Route path="/edit-profile" element={<EditProfile />} />"
+	  <Route path="/add-interest" element={<AddInterest />} />"
+	  <Route path="/choose-courses" element={<ChooseCourses/>} />"
+	  <Route path="/admin" element={<AdminLogin/>} />"
+	  <Route path="/admin-dash" element={<AdminDashboard/>} />"
+	  <Route path="/admin/users" element={<ViewUsers/>} />"
         </Routes>
       </div>
     </Router>
